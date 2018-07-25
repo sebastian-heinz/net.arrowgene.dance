f5218e151a40f463d94070d61ee0000a882110e8
sebastian-heinz <sebastian.heinz@dowjones.com>
2018-07-25 15:40:10 +0800

    rename

--- database/src/main/resources/net/arrowgene/dance/database/mariadb_structure.sql
+++ database/src/main/resources/net/arrowgene/dance/database/mariadb_structure.sql
@@ -6,7 +6,7 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-CREATE TABLE IF NOT EXISTS `dance_database_setting` (
+CREATE TABLE IF NOT EXISTS `database_setting` (
   `key` varchar(255) NOT NULL,
   `value` varchar(255) NOT NULL,
   PRIMARY KEY (`key`)
